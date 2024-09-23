43db871c82446378ac3b810cf12912aadde9edd9
REFAZ212 <rendifauzi212@gmail.com>
2024-09-23 18:33:50 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+nama = "Rendi Fauzi"
+nim = "10223069"
+kelas = "IF III C"
+pesan_dosen = " Terima kasih atas bimbingannya, saya hanya ingin menyampaikan "
+pesan = "Hello World!!"
+
+print("Nama     : ",nama)
+print("NIM      : ",nim)
+print("Kelas    : ",kelas)
+print("==============PESAN==============")
+print(pesan_dosen,pesan)
